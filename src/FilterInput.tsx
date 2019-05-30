097796a98da6290fb1ca8fb5f784ff6edbae9c46
Jonathan Fenske <jonathanfenske@gmail.com>
2019-05-30 15:32:00 -0500

    Honor readOnly

--- src/FilterInput.tsx
+++ src/FilterInput.tsx
@@ -57,6 +57,10 @@ export default class FilterInput extends React.Component<any,any> {
     }
 
     handlePressingAnyCharacter() {
+        if (this.props.additionalCodeMirrorOptions && this.props.additionalCodeMirrorOptions.readOnly === true) {
+            return;
+        }
+
         if (this.autoCompletePopup.completionShow) {
             return;
         }
